4c04f983f1fe749fd36a49aee2bf284e747172a6
Dafne Hernandez <mcflurrycerezita@gmail.com>
2025-12-03 11:36:08 -0600

    feat: ejercicio cambiar logo

--- js-05-oop/src/js/main.js
+++ js-05-oop/src/js/main.js
@@ -1,4 +1,17 @@
 
-document.getElementById("title").innerText = "OOP in JavaScript, Cohorte 62";
+//document.getElementById("title").innerText = "OOP in JavaScript, Cohorte 62";
 
-console.log(`Datos del arreglo participants desde main.js:`, participants); // ['Alice', 'Bob', 'Charlie']
+//console.log(`Datos del arreglo participants desde main.js:`, participants); // ['Alice', 'Bob', 'Charlie']
+
+import { changePrincipalTitle } from "./modules/changePrincipalTitle.js";
+import { changeLogoImage } from "./modules/changeLogoImage.js";
+
+changePrincipalTitle("title");
+
+//para el autocompletado se debe tener el archivo abierto
+
+// Crear una función que se le pase el nombre de la imagen a mostrar (cat o dino)
+// esta imagen es la que se debe mostrar en el elemento con id "logo"
+
+changeLogoImage("guardian");
+//changeImage("cat")
